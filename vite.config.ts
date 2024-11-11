247f6c8665415d50f83c21f85d124eae91c052be
Benedict Adrian <benedictadrianr@gmail.com>
2024-11-11 21:47:43 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  define: {
+    'process.env': {},
+  },
   base: '/Portfolio_Main/',
+  
 })
