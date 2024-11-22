c711b7425fdf45f0f638ec8ebc7d36e669be0937
Benedict Adrian <benedictadrianr@gmail.com>
2024-11-22 10:57:43 +0700

    update responsive

--- src/components/router/index.tsx
+++ src/components/router/index.tsx
@@ -13,11 +13,8 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/Portfolio_Main" element={<App />}>
       <Route index element={<Home />} />
-      <Route path="/Portfolio_Main/experience" element={<Experience />} />
-      <Route
-        path="/Portfolio_Main/experience/:expId"
-        element={<DetailedExperience />}
-      />
+      <Route path="experience" element={<Experience />} />
+      <Route path="experience/:expId" element={<DetailedExperience />} />
       <Route path="*" element={<NotFound />} />
     </Route>
   )
--- src/pages/Home/Experience/DetailedExperience.tsx
+++ src/pages/Home/Experience/DetailedExperience.tsx
@@ -36,7 +36,7 @@ const DetailedExperience = () => {
   }, [expId]);
 
   return (
-    <Section className="!py-0">
+    <Section className="!py-0 overflow-hidden">
       {loading ? (
         <div className="animate-spin text-primary w-[50px]">
           <Loader size={"max"} />
@@ -49,16 +49,19 @@ const DetailedExperience = () => {
             <ArrowLeft className="w-10 h-10" />
             Home
           </div>
-          <div className="px-40">
+          <div className="px-10 xl:px-40">
             <Subtitle>{exp.companyName}</Subtitle>
             <Title>{exp.jobDesc}</Title>
           </div>
-          <div className="w-full max-h-screen overflow-hidden flex justify-center items-center mb-10">
-            <img src={exp.previewImg} className="object-contain" />
+          <div className="w-full flex justify-center items-center mb-10">
+            <img
+              src={exp.previewImg}
+              className="w-full max-h-screen object-cover object-center"
+            />
           </div>
-          <div className="px-40 flex flex-col gap-24">
-            <div className="w-full flex items-center gap-6">
-              <div className="w-1/2">
+          <div className="px-10 xl:px-40 flex flex-col gap-24">
+            <div className="w-full flex flex-col md:flex-row items-center gap-6">
+              <div className="md:w-1/2">
                 <Title className="mb-5">Overview</Title>
                 <Paragraph>{exp.overview}</Paragraph>
               </div>
@@ -66,25 +69,25 @@ const DetailedExperience = () => {
                 <img
                   src={exp.overviewImg}
                   alt={`${exp.companyName} overview image`}
-                  className="w-1/2 pl-6 object-cover object-center"
+                  className="md:w-1/2 md:pl-6 object-cover object-center"
                 />
               )}
             </div>
-            <div className="w-full flex justify-end items-center gap-6">
+            <div className="w-full flex flex-col-reverse md:flex-row justify-end items-center gap-6">
               {exp.highlightImg && (
                 <img
                   src={exp.highlightImg}
                   alt={`${exp.companyName} highlight image`}
-                  className="w-1/2 pr-6 object-cover object-center"
+                  className="md:w-1/2 md:pr-6 object-cover object-center"
                 />
               )}
-              <div className="w-1/2">
-                <Title className="mb-5 text-end">Highlight</Title>
-                <Paragraph className="text-end">{exp.highlight}</Paragraph>
+              <div className="md:w-1/2">
+                <Title className="mb-5 md:text-end">Highlight</Title>
+                <Paragraph className="md:text-end">{exp.highlight}</Paragraph>
               </div>
             </div>
-            <div className="w-full flex items-center gap-6">
-              <div className="w-1/2">
+            <div className="w-full flex flex-col md:flex-row items-center gap-6">
+              <div className="md:w-1/2">
                 <Title className="mb-5">What I Learned the Most</Title>
                 <Paragraph>{exp.learnt}</Paragraph>
               </div>
@@ -92,7 +95,7 @@ const DetailedExperience = () => {
                 <img
                   src={exp.learntImg}
                   alt={`${exp.companyName} overview image`}
-                  className="w-1/2 pl-6 object-cover object-center"
+                  className="md:w-1/2 md:pl-6 object-cover object-center"
                 />
               )}
             </div>
