49bee1d18f99a6df60c2585d3c4a9746db5a49aa
Benedict Adrian <benedictadrianr@gmail.com>
2025-05-18 21:28:16 +0700

    feat: update personal project data with new images and add i-Meeting PLN project

--- src/features/project/personal/data/index.ts
+++ src/features/project/personal/data/index.ts
@@ -1,5 +1,6 @@
 import { projectType } from "../../type";
 
+import weddingbg from "/img/project/personal/background/bmi.jpg";
 import wedding1 from "/img/project/personal/carousel/wedding1.png";
 import wedding2 from "/img/project/personal/carousel/wedding2.png";
 import wedding3 from "/img/project/personal/carousel/wedding3.png";
@@ -7,33 +8,60 @@ import wedding4 from "/img/project/personal/carousel/wedding4.png";
 import wedding5 from "/img/project/personal/carousel/wedding5.png";
 import wedding6 from "/img/project/personal/carousel/undangan-pernikahan-digital.png";
 
+import nflixbg from "/img/project/personal/background/netflix.jpg";
 import nflix1 from "/img/project/personal/carousel/nflix1.png";
 import nflix2 from "/img/project/personal/carousel/nflix2.png";
 import nflix3 from "/img/project/personal/carousel/nflix3.png";
 import nflix4 from "/img/project/personal/carousel/nflix4.png";
 import nflix5 from "/img/project/personal/carousel/netflix.jpg";
 
+import trombonebg from "/img/project/personal/background/landingpage.jpg";
 import trombone1 from "/img/project/personal/carousel/landingpage.jpg";
 import trombone2 from "/img/project/personal/carousel/trombone1.png";
 
+import bmibg from "/img/project/personal/background/bmi.jpg";
 import bmi from "/img/project/personal/carousel/bmi1.png";
 import bmi2 from "/img/project/personal/carousel/bmi2.png";
 
+import mushroombg from "/img/project/personal/background/mushroom.png";
 import mushroom from "/img/project/personal/carousel/mushroom.png";
 import mushroom1 from "/img/project/personal/carousel/mushroom1.png";
 import mushroom2 from "/img/project/personal/carousel/mushroom2.png";
 
+import tributebg from "/img/project/personal/carousel/tribute.jpg";
 import tribute from "/img/project/personal/carousel/tribute.jpg";
 import tribute1 from "/img/project/personal/carousel/tribute1.png";
 import tribute2 from "/img/project/personal/carousel/tribute2.png";
 import tribute3 from "/img/project/personal/carousel/tribute3.png";
 
+import pln1 from "/img/project/personal/carousel/pln1.png";
+import pln2 from "/img/project/personal/carousel/pln2.png";
+import pln3 from "/img/project/personal/carousel/pln3.png";
+import pln4 from "/img/project/personal/carousel/pln4.png";
+
 export const personalProjectData: projectType[] = [
     {
         id: 1,
+        name: "i-Meeting PLN",
+        link: "https://imeeting-pln.vercel.app/",
+        img: pln1,
+        skillSet: [
+            "Next.js",
+            "Typescript",
+            "Drizzle",
+        ],
+        imgCarousel: [
+            pln1,
+            pln2,
+            pln3,
+            pln4,
+        ],
+    },
+    {
+        id: 2,
         name: "Wedding Invitation",
         link: "https://benedictadrianr.github.io/Undangan-Pernikahan-Digital/",
-        img: "https://egwlwjugyuksxfwceoqy.supabase.co/storage/v1/object/public/Portfolio/Project_Images/undangan-pernikahan-digital.png",
+        img: weddingbg,
         skillSet: [
             "HTML",
             "Javascript",
@@ -49,10 +77,10 @@ export const personalProjectData: projectType[] = [
         ],
     },
     {
-        id: 2,
+        id: 3,
         name: "NFlix Landing Page",
         link: "https://benedictadrianr.github.io/nflixlandpage/",
-        img: "https://egwlwjugyuksxfwceoqy.supabase.co/storage/v1/object/public/Portfolio/Project_Images/netflix.jpg?t=2024-08-06T07%3A43%3A40.034Z",
+        img: nflixbg,
         skillSet: [
             "HTML",
             "CSS",
@@ -67,10 +95,10 @@ export const personalProjectData: projectType[] = [
         ],
     },
     {
-        id: 3,
+        id: 4,
         name: "Trombone Landing Page",
         link: "https://benedictadrianr.github.io/product-landing-page/",
-        img: "https://egwlwjugyuksxfwceoqy.supabase.co/storage/v1/object/public/Portfolio/Project_Images/landingpage.jpg?t=2024-08-06T07%3A55%3A02.663Z",
+        img: trombonebg,
         skillSet: ["HTML", "CSS", "Javascript"],
         imgCarousel: [
             trombone1,
@@ -78,11 +106,11 @@ export const personalProjectData: projectType[] = [
         ],
     },
     {
-        id: 4,
+        id: 5,
         name: "BMI Calculator",
         link:
             "https://revou-fundamental-course.github.io/10-jul-23-rianrich123/",
-        img: bmi,
+        img: bmibg,
         skillSet: [
             "HTML",
             "CSS",
@@ -94,10 +122,10 @@ export const personalProjectData: projectType[] = [
         ],
     },
     {
-        id: 5,
+        id: 6,
         name: "Gameshop Website",
         link: "https://benedictadrianr.github.io/mushroom-gaming",
-        img: "https://egwlwjugyuksxfwceoqy.supabase.co/storage/v1/object/public/Portfolio/Project_Images/mushroom.png?t=2024-08-06T07%3A49%3A12.440Z",
+        img: mushroombg,
         skillSet: [
             "ReactJS",
             "TailwindCSS",
@@ -109,10 +137,10 @@ export const personalProjectData: projectType[] = [
         ],
     },
     {
-        id: 6,
+        id: 7,
         name: "Biography",
         link: "https://benedictadrianr.github.io/stephen-king-tribute-page/",
-        img: "https://egwlwjugyuksxfwceoqy.supabase.co/storage/v1/object/public/Portfolio/Project_Images/tribute.jpg?t=2024-08-06T07%3A51%3A55.237Z",
+        img: tributebg,
         skillSet: [
             "HTML",
             "CSS",
